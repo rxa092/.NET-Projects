1439321071e37d5a6938cf0459e92414b844121a
Muhammad Raza <raza19984@gmail.com>
2022-09-03 14:47:54 +0500

    Kitchen

---
+++ Kitchen/Kitchen/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Kitchen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_orders = new System.Windows.Forms.TextBox();
+            this.lbl_orders = new System.Windows.Forms.Label();
+            this.txt_completed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_completed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_orders
+            // 
+            this.txt_orders.Location = new System.Drawing.Point(113, 51);
+            this.txt_orders.Multiline = true;
+            this.txt_orders.Name = "txt_orders";
+            this.txt_orders.ReadOnly = true;
+            this.txt_orders.Size = new System.Drawing.Size(401, 360);
+            this.txt_orders.TabIndex = 0;
+            // 
+            // lbl_orders
+            // 
+            this.lbl_orders.AutoSize = true;
+            this.lbl_orders.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_orders.Location = new System.Drawing.Point(243, 9);
+            this.lbl_orders.Name = "lbl_orders";
+            this.lbl_orders.Size = new System.Drawing.Size(120, 39);
+            this.lbl_orders.TabIndex = 1;
+            this.lbl_orders.Text = "Orders";
+            // 
+            // txt_completed
+            // 
+            this.txt_completed.Location = new System.Drawing.Point(631, 186);
+            this.txt_completed.Name = "txt_completed";
+            this.txt_completed.Size = new System.Drawing.Size(100, 22);
+            this.txt_completed.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(556, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Completed Order";
+            // 
+            // btn_completed
+            // 
+            this.btn_completed.Location = new System.Drawing.Point(631, 226);
+            this.btn_completed.Name = "btn_completed";
+            this.btn_completed.Size = new System.Drawing.Size(104, 39);
+            this.btn_completed.TabIndex = 4;
+            this.btn_completed.Text = "Submit";
+            this.btn_completed.UseVisualStyleBackColor = true;
+            this.btn_completed.Click += new System.EventHandler(this.btn_completed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_completed);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_completed);
+            this.Controls.Add(this.lbl_orders);
+            this.Controls.Add(this.txt_orders);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_orders;
+        private System.Windows.Forms.Label lbl_orders;
+        private System.Windows.Forms.TextBox txt_completed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_completed;
+    }
+}
+
---
+++ Kitchen/Kitchen/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitchen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TcpListener listner = new TcpListener(new IPEndPoint(IPAddress.Loopback, 11000));
+            listner.Start();
+            listner.BeginAcceptTcpClient(new AsyncCallback(ConnectListner), listner);
+
+        }
+        TcpClient client;
+        Dictionary<String, TcpClient> Clients = new Dictionary<string, TcpClient>();
+        static byte[] msg = new byte[1024];
+        List<string> orders = new List<string>();
+        List<string> waiting = new List<string>();
+
+        private void ConnectListner(IAsyncResult ar)
+        {
+            TcpListener listner = (TcpListener)ar.AsyncState;
+            TcpClient Client = listner.EndAcceptTcpClient(ar);
+            NetworkStream ns = Client.GetStream();
+            int count = ns.Read(msg, 0, msg.Length);
+            string name = (ASCIIEncoding.ASCII.GetString(msg, 0, count));
+            Clients.Add(name, Client);
+            ns.BeginRead(msg, 0, msg.Length, new AsyncCallback(StartRead), ns);
+            listner.BeginAcceptTcpClient(new AsyncCallback(ConnectListner), listner);
+
+        }
+
+
+
+
+        private void StartRead(IAsyncResult ar)
+        {
+            try
+            {
+                NetworkStream ns = client.GetStream();
+                int count = ns.EndRead(ar);
+                string msg1 = ASCIIEncoding.ASCII.GetString(msg, 0, count);
+                string[] orderarray = msg1.Split(',');
+                //InboxTxt.Text += msg1;
+                //InboxTxt.Text += Environment.NewLine;
+                if (orders.Count < 5)
+                {
+                    orders.Add(msg1);
+                    for (int i = 0; i < orders.Count; i++)
+                    {
+                        txt_orders.Text += orders[i];
+                        txt_orders.Text += Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    waiting.Add(msg1);
+                    TcpClient waitingclient = new TcpClient();
+                    Clients.TryGetValue("waiting", out waitingclient);
+                    NetworkStream nswaiting = waitingclient.GetStream();
+                    nswaiting.Write(ASCIIEncoding.ASCII.GetBytes(orderarray[0]), 0, orderarray[0].Length);
+                }
+                ns.BeginRead(msg, 0, msg.Length, new AsyncCallback(StartRead), ns);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void btn_completed_Click(object sender, EventArgs e)
+        {
+            TcpClient completedclient = new TcpClient();
+            Clients.TryGetValue("completed", out completedclient);
+            NetworkStream completedstream = completedclient.GetStream();
+            completedstream.Write(ASCIIEncoding.ASCII.GetBytes(txt_completed.Text), 0, txt_completed.Text.Length);
+            for (int i = 0; i < orders.Count; i++)
+            {
+                string[] temp = orders[i].Split(',');
+                if (txt_completed.Text.ToString() == temp[0])
+                {
+                    orders.RemoveAt(i);
+                    orders.Add(waiting[0]);
+                }
+            }
+            string[] arr = waiting[0].Split(',');
+            TcpClient waitingclient = new TcpClient();
+            Clients.TryGetValue("waiting", out waitingclient);
+            NetworkStream waitingstream = waitingclient.GetStream();
+            waitingstream.Write(ASCIIEncoding.ASCII.GetBytes(arr[0]), 0, arr[0].Length);
+        }
+    }
+}
